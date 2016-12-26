132018c08c982d1981015353eaf331aa91c00cae
Jorge Juan Ramos Garnero <jorge.ramos@me.com>
2016-12-26 18:58:47 +0100

    upgraded BusCalendarItem

--- MadridBUS/Source/Data/BusCalendarItem.swift
+++ MadridBUS/Source/Data/BusCalendarItem.swift
@@ -4,12 +4,24 @@ import ObjectMapper
 enum BusDayType: String {
     case labour = "LA"
     case saturday = "SA"
+    case friday = "V"
     case holiday = "FE"
+    case undetermined = ""
+}
+
+enum BusSeason: String {
+    case winter = "IV"
+    case springHolidays = "SS"
+    case christmas = "NV"
+    case lowSeason = "BJ"
+    case summer = "VN"
+    case undetermined = ""
 }
 
 final class BusCalendarItem: Mappable {
     var date: Date = Date()
-    var dayType: BusDayType = .labour
+    var dayType: BusDayType = .undetermined
+    var season: BusSeason = .undetermined
     var onStrike: Bool = false
     
     required init?(map: Map) {}
@@ -20,6 +32,7 @@ final class BusCalendarItem: Mappable {
         
         date <- (map["date"], DateFormatterTransform(dateFormatter: dateFormatter))
         dayType <- (map["dayType"], EnumTransform<BusDayType>())
+        season <- (map["seasonTg"], EnumTransform<BusSeason>())
         onStrike <- map["strike"]
     }
 }
